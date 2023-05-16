d7c299d7c8528f8da754bec3954095e832e296b6
Michael Stewart <123015728+MykeStu@users.noreply.github.com>
2023-05-16 14:40:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Array Methods Practice
+
+To do these exercises, click on `Use this template`, then use `git clone <url for your new repo>` in your terminal. 
+
+To check your answers, run `node main.js` in the same directory that the repo is in to see the logged results. If you only want to see one result at a time, you can comment out the results that you've already checked. 
---
+++ main.js
@@ -0,0 +1,36 @@
+const contacts = require('./data.json');
+
+// the following exercises require the data from data.json.
+// you can look in that file to see the data structure you
+// will be working with. 
+
+// For each exercise:
+// 1. use array methods with the variable contacts to get the desired result, 
+// 2. store the result with a variable
+// 3. appropriately log the results to the console
+
+
+
+//You can put your solutions after each comment below:
+
+//1. an array that contains all of the contacts that work at INSECTUS
+
+//2. an array all of the contacts, with only the name property
+
+//3. an array of all of the contacts over the age of 50
+
+//4. the first ten contacts when alphabetically ordered by name
+
+//5. the oldest person's name
+
+//6. the contact id with the name Isabella Burke
+
+//7. an array of all of the contacts, but with the name split up into a firstName
+// and lastName properties
+
+//8. an array of all of the contacts where the friends property
+// is an array with each contact that is their friend
+
+//9. the average age of the contacts
+
+//10. the median age of the contacts
